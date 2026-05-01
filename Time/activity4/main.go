6ce2cb357feeff73709ff0de41372287b574fb57
iambronnix <erickndeto202@gmail.com>
2026-05-01 19:11:53 +0300

    comparing time

--- Time/activity4/main.go
+++ Time/activity4/main.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	now := time.Now()
-	onlyAafter, err := time.Parse(time.RFC3339, "2026-5-1:19:41+00:00")
+	onlyAfter, err := time.Parse(time.RFC3339, "2020-5-1:19:00:00:00")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(now, onlyAafter)
-	fmt.Println(now.After(onlyAafter))
-	if now.After(onlyAafter) {
+	fmt.Println(now, onlyAfter)
+	fmt.Println(now.After(onlyAfter))
+	if now.After(onlyAfter) {
 		fmt.Println("I'm DEADPOOL.....Executing actions!!!")
 	} else {
 		fmt.Println("Now is not the time yet jokerr!!!")
