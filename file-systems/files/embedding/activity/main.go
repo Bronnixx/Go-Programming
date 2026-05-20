c5ce55801b14f92e17e1192b15f4ecf85cbce363
iambronnix <erickndeto202@gmail.com>
2026-05-20 19:07:21 +0300

    embedding a text

---
+++ file-systems/files/embedding/activity/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"embed"
+	"os"
+	"text/template"
+)
+type Person struct{
+	Name string
+}
+var (
+	//go:embed templates
+	f embed.FS
+)
+func main(){
+	p := Person{"Erick"}
+	tmpl, err := template.ParseFS(f, "templates/template.txt")
+	if err != nil{
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, p)
+	if err != nil{
+		panic(err)
+	}
+}
